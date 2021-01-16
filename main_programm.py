051d049c78f355b2fe353c4f4db5673163d18afe
aldsouza4 <aldsouza4@gmail.com>
2021-01-16 12:49:20 +0300

    structured the code using OOP

--- main_programm.py
+++ main_programm.py
@@ -4,110 +4,107 @@ from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st = int(input("enter no. of stocks:- \n "))
-assets = []
-for i in range(0, st):
-    x = input("enter stock ticker:- ")
-    assets.append(x)
 
-pf_data = pd.DataFrame()
-for a in assets:
-    pf_data[a] = wb.DataReader(a, data_source='yahoo', start='2010-1-1')['Adj Close']
+class PortfolioOptimization(object):
 
-(pf_data / pf_data.iloc[0] * 100).plot(figsize=(10, 5))
-log_returns = np.log(pf_data / pf_data.shift(1))
+    def __init__(self):
+        self.stock_list = []
 
-# log_returns.mean() * 250
-# log_returns.cov() * 250
+    def enter_stocks(self):
+        # self.num_stocks = int(input("enter no. of stocks:- \n "))
+        # for i in range(num_stocks):
+        #     x = input("Enter ticker of stock {} :-".format(i))
+        #     self.stock_list.append(x)
+        self.stock_list = ['STLTECH.NS', 'RAIN.NS', 'TCS.NS']
+        self.num_stocks = len(self.stock_list)
 
-num_assets = len(assets)
+    def get_data(self):
+        self.enter_stocks()
 
-num_ports = 1000
-pfolio_returns = []
-pfolio_volatilities = []
-all_weights = np.zeros((num_ports, num_assets))
-sharpe_array = np.zeros(num_ports)
-for x in range(num_ports):
-    weights = np.random.random(num_assets)
-    weights /= np.sum(weights)
-    pfolio_returns.append(np.sum(weights * log_returns.mean()) * 250)
-    pfolio_volatilities.append(np.sqrt(np.dot(weights.T, np.dot(log_returns.cov() * 250, weights))))
-    all_weights[x, :] = weights
-    sharpe_array[x] = pfolio_returns[x] / pfolio_volatilities[x]
+        self.pf_data = pd.DataFrame()
+        for a in self.stock_list:
+            self.pf_data[a] = wb.DataReader(a, data_source='yahoo', start='2011-1-1')['Adj Close']
 
-pfolio_returns = np.array(pfolio_returns)
-pfolio_volatilities = np.array(pfolio_volatilities)
-all_weights = np.array(all_weights)
-sharpe_array = np.array(sharpe_array)
-print('\n Maximum Sharpe Ratio in the array: {}'.format(sharpe_array.max()))
+    def log_returns(self):
+        self.get_data()
 
-k = sharpe_array.argmax()
+        (self.pf_data / self.pf_data.iloc[0] * 100).plot(figsize=(10, 5))
+        self.log_returns = np.log(self.pf_data / self.pf_data.shift(1))
 
-max_sr_returns = pfolio_returns[sharpe_array.argmax()]
-max_sr_volatilities = pfolio_volatilities[sharpe_array.argmax()]
-plt.figure(figsize=(12, 8))
-plt.scatter(pfolio_volatilities, pfolio_returns, c=sharpe_array, cmap='viridis')
-plt.colorbar(label='Sharpe Ratio')
-plt.xlabel('Volatility')
-plt.ylabel('Return')
-plt.scatter(max_sr_volatilities, max_sr_returns, c='red', s=50, edgecolors='black')
-plt.show()
-new_weights = all_weights[k, :] * 100
-for i in range(0, st):
-    print("Weight of {:<15} is {:<6} % ".format(assets[i], round(new_weights[i], 2)))
+    def portfolio_simulation(self, num_ports=1000, show = True):
+        self.log_returns()
+
+        num_ports = 1000
+        self.pfolio_returns = []
+        self.pfolio_volatilities = []
+        self.all_weights = np.zeros((num_ports, self.num_stocks))
+        sharpe_array = np.zeros(num_ports)
+
+        for x in range(num_ports):
+            weights = np.random.random(self.num_stocks)
+            weights /= np.sum(weights)
+            self.pfolio_returns.append(np.sum(weights * self.log_returns.mean()) * 250)
+            self.pfolio_volatilities.append(
+                np.sqrt(np.dot(weights.transpose(), np.dot(self.log_returns.cov() * 250, weights))))
+            self.all_weights[x, :] = weights
+            sharpe_array[x] = self.pfolio_returns[x] / self.pfolio_volatilities[x]
+
+        self.pfolio_returns = np.array(self.pfolio_returns)
+        self.pfolio_volatilities = np.array(self.pfolio_volatilities)
+        self.all_weights = np.array(self.all_weights)
+        self.sharpe_array = np.array(sharpe_array)
+        self.max_sr_returns = self.pfolio_returns[self.sharpe_array.argmax()]
+        self.max_sr_volatilities = self.pfolio_volatilities[self.sharpe_array.argmax()]
+
+        if show:
+            print('\n Maximum Sharpe Ratio in the array: {}'.format(self.sharpe_array.max()))
+
+    def plot(self):
+        self.portfolio_simulation(show=False)
+
+        plt.figure(figsize=(12, 8))
+        plt.scatter(self.pfolio_volatilities, self.pfolio_returns*100, c=self.sharpe_array, cmap='viridis')
+        plt.colorbar(label='Sharpe Ratio')
+        plt.xlabel('Volatility (%)')
+        plt.ylabel('Return (%)')
+        plt.scatter(self.max_sr_volatilities, self.max_sr_returns*100, c='red', s=50, edgecolors='black')
+        plt.title("Volatility vs return")
+        plt.show()
+
+    def weight_optimized_pft(self):
+        self.portfolio_simulation(show=False)
+
+        new_weights = self.all_weights[self.sharpe_array.argmax(), :] * 100
+        for i in range(self.num_stocks):
+            print("Weight of {:<15} is {:<6} % ".format(self.stock_list[i], round(new_weights[i], 2)))
+
+    def final_result(self, plot = False):
+        if plot:
+            self.plot()
+
+        self.weight_optimized_pft()
+
+        new_weights = self.all_weights[self.sharpe_array.argmax(), :] * 100
+        ret = np.sum(self.log_returns.mean() * new_weights) * 250
+        vol = np.sqrt(np.dot(new_weights.T, np.dot(self.log_returns.cov() * 250, new_weights)))
+
+        sr = ret / vol
+        equal_weight = 1 / self.num_stocks
+        equal_weights_list = []
+        for i in range(self.num_stocks):
+            equal_weights_list.append(equal_weight)
+
+        ret_equal = np.sum(equal_weights_list * self.log_returns.mean()) * 250
+        voltis = np.sqrt(np.dot(equal_weights_list, np.dot(self.log_returns.cov() * 250, equal_weights_list)))
+
+        print("\n The EXPECTED return is {} %".format(round(ret, 2)))
+        print("\n The EXPECTED volatility is {} %".format(round(vol, 2)))
+        print("\n The MAX sharpe ratio is {} \n ".format(round(sr, 2)))
+
+        print("The return if All the STOCKS have equal Weightage is {}  % \n ".format(round(ret_equal * 100, 2)))
+        print("The Volatility if All the STOCKS have equal Weightage is {} % \n ".format(round(voltis * 100, 2)))
 
 
-def get_ret_vol_sr(weights):
-    weights = np.array(weights)
-    ret = np.sum(log_returns.mean() * weights) * 250
-    vol = np.sqrt(np.dot(weights.T, np.dot(log_returns.cov() * 250, weights)))
-    sr = ret / vol
-    return np.array([ret, vol, sr])
-
-
-def neg_sharpe(weights):
-    return get_ret_vol_sr(weights)[2] * -1
-
-
-def check_sum(weights):
-    return np.sum(weights) - 1
-
-
-cons = ({'type': 'eq', 'fun': check_sum})
-bounds = []
-for i in range(0, st):
-    x = (0, 1)
-    bounds.append(x)
-tuple(bounds)
-xx = 1 / st
-initial_guess = []
-for i in range(0, num_assets):
-    initial_guess.append(xx)
-
-from scipy.optimize import minimize
-
-opt_results = minimize(neg_sharpe, initial_guess, method='SLSQP', bounds=bounds, constraints=cons)
-
-opt_results.x
-fresult = get_ret_vol_sr(opt_results.x)
-
-print("\n The EXPECTED return is {} %".format(round(fresult[0] * 100, 2)))
-print("\n The EXPECTED volitility is {} %".format(round(fresult[1] * 100, 2)))
-print("\n  The MAX sharpe ratio is {} \n ".format(round(fresult[2], 2)))
-
-weight_1 = 1 / st
-
-
-weights_1 = []
-for i in range(0, num_assets):
-    weights_1.append(weight_1)
-
-ret = np.sum(weights_1 * log_returns.mean()) * 250
-
-print("The return if All the STOCKS have equal Weightage is {}  % \n ".format(round(ret * 100, 2)))
-
-var = np.dot(weights_1, np.dot(log_returns.cov() * 250, weights_1))
-
-voltis = np.sqrt(np.dot(weights_1, np.dot(log_returns.cov() * 250, weights_1)))
-
-print("The Volitility if All the STOCKS have equal Weightage is {} % \n ".format(round(voltis * 100, 2)))
+if __name__ == '__main__':
+    t = PortfolioOptimization()
+    t.final_result()
